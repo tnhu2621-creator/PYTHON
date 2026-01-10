172ab03c0dd44d13cdd49561ce6d839f03ade3a6
Nguyen Hung <info.hungplay@gmail.com>
2026-01-10 15:06:57 +0700

    Initial commit

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-
 @login_manager.user_loader
 def load_user(id):
     return NguoiDung.query.get(int(id))
